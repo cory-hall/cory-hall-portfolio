96f4c8fd711fe4d81e3ff4eb80b2bf0db9cf4230
Cory Hall <comebackcory1@gmail.com>
2022-07-19 22:58:14 -0500

    homepage styled, need to add navbar for responsive design

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,40 @@ import Link from 'next/link';
 
 import styles from '../styles/Home.module.css'
 
+import portPic from '../public/portPicBNW.png'
+
 export default function Home() {
   return (
-    <section className='flex flex-col flex-wrap'>
-      <div className='w-auto'>
-        <Link className='w-auto' href='/about'>
-          <p className={styles.aboutLink + " before:content-['Hello.']"}></p>
-        </Link>
+    <section className={styles.homeDiv + ' flex'}>
+
+      <div className={styles.linksDiv + ' flex md:flex-col justify-center w-1/2 h-full lg:pl-24 xl:pl-48'}>
+        <div>
+          <Link href='/about'>
+            <a className={styles.aboutLink + " w-auto before:content-['Hello']"}></a>
+          </Link>
+        </div>
+
+        <div>
+          <Link href='projects'>
+            <a className={styles.projectsLink + " before:content-['My_Name']"}></a>
+          </Link>
+        </div>
+
+        <div>
+          <Link href='/contact'>
+            <a className={styles.contactLink + " before:content-['Is_Cory']"}></a>
+          </Link>
+        </div>
       </div>
 
-      <div>
-        <Link href='/projects'>
-          <h1 className={styles.projectsLink + " before:content-['My_Name']"}></h1>
-        </Link>
+      <div className='flex justify-center w-1/2 h-full mr-8'>
+        <Image
+        src={portPic}
+        objectFit='contain'
+        >
+
+        </Image>
       </div>
-      <Link href='/contact'>
-        <h1 className={styles.contactLink + " before:content-['Is_Cory']"}></h1>
-      </Link>
     </section>
   )
 }
