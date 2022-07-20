4e624f74a50bbea93d44d052cc8d9e80a95d1ba6
Cory Hall <comebackcory1@gmail.com>
2022-07-19 21:34:53 -0500

    added links to other pages

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link';
 
-import About from './about';
-import Contact from './contact';
-import Projects from './projects';
-
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <section className={styles.homeDiv}>
-      <Link href='/about'>
-        <h1>Hello.</h1>
+    <section className='flex flex-col flex-wrap'>
+      <div className='w-auto'>
+        <Link className='w-auto' href='/about'>
+          <p className={styles.aboutLink + " before:content-['Hello.']"}></p>
+        </Link>
+      </div>
+
+      <div>
+        <Link href='/projects'>
+          <h1 className={styles.projectsLink + " before:content-['My_Name']"}></h1>
+        </Link>
+      </div>
+      <Link href='/contact'>
+        <h1 className={styles.contactLink + " before:content-['Is_Cory']"}></h1>
       </Link>
     </section>
   )
