6b7001c1990b2fc346eb0f995813e37f875b9edc
Cory Hall <comebackcory1@gmail.com>
2022-07-30 23:56:24 -0500

    fixed footer hover, header links styled better

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,20 @@
 import { Link } from "react-scroll";
 
+import upArrow from '../../assets/images/scroll-up-50.png'
+
 function Footer() {
 
   return (
-    <div className="footerComp flex items-center justify-center w-full">
-      <h1 className="">&copy; 2022 Cory Hall</h1>
+    <div className="footerComp flex items-center justify-between w-full">
+        <h1 className="footerText pl-12">&copy; 2022 Cory Hall</h1>
+        <Link
+          className='pageLink footerLink'
+          to='landingSection'
+          spy={true}
+          smooth={true}
+          duration={500}>
+          <img src={upArrow} alt='up arrow'></img>
+        </Link>
     </div>
   )
 }
