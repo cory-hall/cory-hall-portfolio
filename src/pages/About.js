a9bebc7bbf91d404bd94abe876a5c53cd7c4e856
Cory Hall <comebackcory1@gmail.com>
2022-07-31 11:22:00 -0500

    fleshing responsive design

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,26 @@
-import { Link } from 'react-scroll'
-
-import upArrow from '../assets/images/scroll-up-50.png'
+import portPic from '../assets/images/portPicBNW.png'
 
 function About() {
 
   return (
     
-    <section className='aboutPage flex flex-col' id='aboutSection'>
-      <div className='flex justify-end w-full'>
-      <Link
-          className='pageLink'
-          to='landingSection'
-          spy={true}
-          smooth={true}
-          duration={500}>
-          <img src={upArrow} alt='up arrow'></img>
-        </Link>
+    <section className='aboutPage flex justify-center items-center w-full' id='aboutSection'>
+      <div className='flex m-24 w-1/2'>
+        <p>
+          I am a full stack web developer specializing in the MERN stack and a recent
+          recipiant of a full stack web development certificate from Michigan State University.
+          I am also a part time student working towards obtaining a Bachelor's Degree in Computer Science.
+          <br></br>
+          <br></br>
+          Through my years of working, mainly in the service industry, I have aquired numerous skills that
+          cannot be taught and are typically only obtained by those that put years within said industry. I 
+          fully plan on leveraging these skills to help create unique experiences from someone who knows exactly
+          what the customer wants.
+        </p>
       </div>
-      <p>ABOUT ME</p>
+      <div className='w-1/2 m-24'>
+        <img src={portPic} alt='Cory Hall'></img>
+      </div>
     </section>
   )
 }
